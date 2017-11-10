8946bcb5ee999e576b58c896f8927305000d3b52
Ivan Dymkov <mstrdymio@gmail.com>
2017-11-10 22:48:07 +0700

    Add public section

---
+++ public/books-catalog/index.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "Каталог книг");
+$APPLICATION->SetTitle("Библиотека");
+?>
+  <p>Проверка.</p>
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
